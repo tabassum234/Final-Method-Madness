36ab09f52ce9b94d7b5a76bd59a90e70f055f175
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2017-10-26 08:55:39 -0400

    Final Method Madness

---
+++ Runner.java
@@ -0,0 +1,10 @@
+
+public class Runner
+{
+	public static void main (String [] args) {
+		LZouLib.sumUpTo(8);
+		LZouLib.multiplicationTable(10);
+		LZouLib.isPalindrome("1441");
+	}
+}
+
---
+++ Runner2.java
@@ -0,0 +1,14 @@
+// Tabassum Bhuiyan Period 2 Runner Class
+public class Runner
+{
+	public static void main (String []args)
+	{
+		int check = 5;
+		System.out.println(TBhuiyanLib.isFibonacci(check));
+		String word2 = "cook";
+		String word = "bold";
+		System.out.println(TBhuiyanLib.cutOut(word, word2));
+		String date = "10/13/2017";
+		System.out.println(TBhuiyanLib.dateStrstring(date));
+	}
+}
